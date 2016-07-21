17a5d757445ce2a73df8a055150c9a0ad8db28b4
Yanis <yanis.urbis@gmail.com>
2016-07-21 08:23:30 +0300

    Functions.

--- Main.elm
+++ Main.elm
@@ -1,7 +1,35 @@
 module Main exposing (..)
 
-import Html     
+import Html  
+import String exposing (length, toUpper)   
 
 -- main should be html, svg or program
 main = 
-    Html.text "Hello World"
+    Html.text (exercise2 "James Moore")
+
+add a b =
+    a + b
+
+rezult1 =
+    add (add 1 2) 3
+
+-- partial functions
+-- pipe operator
+rezult2 =
+    add 1 2
+    |> add 3
+
+rezult3 = 
+    add 1 2
+    |> \a -> a % 2 == 0
+
+exercise2 : String -> String
+exercise2 string = 
+    if length string > 10 then
+        toUpper string 
+        ++ " - name length: " 
+        ++ toString (length string)
+    else
+        string 
+        ++ " - name length: " 
+        ++ toString (length string)
