d64e848b108de63695da60183141c356e09d997c
Yanis <yanis.urbis@gmail.com>
2016-07-27 10:02:57 +0300

    Second draft.

--- ScoreKeeper.elm
+++ ScoreKeeper.elm
@@ -6,7 +6,7 @@ import Html.Events exposing (..)
 import Html.App as Html
 import String
 import String exposing (length, toInt)
-import Array exposing (Array, fromList, push, get, set, indexedMap)
+import Array exposing (Array, fromList, push, get, set, indexedMap, map)
 import List 
 
 -- first part MODEL
@@ -16,15 +16,23 @@ type alias Player =
     , points : Int 
     }
 
+type alias Play = 
+    { name : String
+    , points : Int
+    , playerIndex : Int
+    }
+
 type alias Model =
     { players : Array Player
     , input : String
+    , plays : Array Play
     }
 
 initModel : Model
 initModel = 
     { players = fromList []
     , input = ""
+    , plays = fromList []
     }
 
 updatePlayerScore : Array Player -> Int -> Int -> Array Player
@@ -35,21 +43,59 @@ updatePlayerScore players index points =
                 index
                 { player | points = player.points + points }
                 players
+
         Nothing ->
             players
 
 -- second part UPDATE
+
+type alias Index
+    = Int
+
+type alias Point
+    = Int
+
 type Msg
     = AddPlayer
     | Add3Points Int
     | Add2Points Int
     | Input String
+    | DeletePlay Index Point
+
+updatePlays : Array Player -> Array Play -> Int -> Int -> Array Play
+updatePlays players plays index points = 
+    -- get name
+    -- make str
+    -- push this str
+    -- return 
+    let 
+        name = case get index players of
+                Just player ->
+                    player.name
+                    
+                Nothing ->
+                    "Error with name"
+    in
+        push
+            (Play name points index)
+            plays 
+    
 
 
 
 update : Msg -> Model -> Model
 update msg model = 
     case msg of
+
+        DeletePlay index points ->
+            { model
+                | players =
+                    updatePlayerScore 
+                        model.players 
+                        index 
+                        -points
+            }
+
         AddPlayer ->
             { model
                 | players = 
@@ -58,18 +104,38 @@ update msg model =
                         , points = 0
                         } 
                         model.players
+                , input = initModel.input
             }
+
         
         Add2Points index ->
             { model
                 | players =
-                    updatePlayerScore model.players index 2
+                    updatePlayerScore 
+                        model.players 
+                        index 
+                        2
+                , plays =
+                    updatePlays
+                        model.players
+                        model.plays
+                        index
+                        2
             }
         
         Add3Points index -> 
             { model
                 | players =
-                    updatePlayerScore model.players index 3
+                    updatePlayerScore 
+                        model.players 
+                        index 
+                        3
+                , plays =
+                    updatePlays
+                        model.players
+                        model.plays
+                        index
+                        3
             }
 
         Input name ->
@@ -77,7 +143,25 @@ update msg model =
                 | input = name
             }
 
+renderPlay : Play -> Html Msg
+renderPlay play =
+    div []
+        [ text 
+            ("Name : " ++ play.name ++ " , Points : " ++ toString play.points ++ " index: " ++ toString play.playerIndex)
+        , button 
+            [ type' "button"
+            , onClick (DeletePlay play.playerIndex play.points)    
+            ]
+            [ text "Delete Play"]
+        ]
 
+renderPlays : Array Play -> Html Msg
+renderPlays plays = 
+    div []
+        ( plays
+            |> map (\ elm -> renderPlay elm)
+            |> Array.toList
+        )
 
 
 renderPlayer : Player -> Int -> Html Msg
@@ -85,7 +169,7 @@ renderPlayer player index =
     div []
         [ h2
             []
-            [ text player.name ]
+            [ text ("Name : " ++ player.name ++ " , Points : " ++ toString player.points) ]
         , button
             [ type' "button"
             , onClick (Add3Points index)
@@ -125,6 +209,7 @@ view model =
             , onClick AddPlayer
             ]
             [ text "Add Player" ]
+        , renderPlays model.plays
         ]
 
 main =
