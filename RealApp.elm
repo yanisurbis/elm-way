1b2ab759f9be65dd9ce763d9c8324a10be0960bf
Yanis <yanis.urbis@gmail.com>
2016-07-27 04:53:06 +0300

    Changes in counter.

--- RealApp.elm
+++ RealApp.elm
@@ -5,67 +5,91 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Html.App as Html
 import String
-import String exposing (length)
+import String exposing (length, toInt)
 
 -- first part MODEL
 type alias Model =
     { calories : Int
-    , inputValue : String
+    , inputValue : Int
+    , error : Maybe String
     }
 
 initModel : Model
 initModel = 
     { calories = 0
-    , inputValue = ""
+    , inputValue = 0
+    , error = Nothing
     }
 
 -- second part UPDATE
 type Msg
-    = AddCalorie Int
+    = AddCalorie
     | Clear
-    | ChangedInput String
+    | Input String
 
 update : Msg -> Model -> Model
 update msg model = 
     case msg of
-        AddCalorie val ->
-            { model | calories = model.calories + val }
+        AddCalorie ->
+            { model | calories = model.calories + model.inputValue
+                    , inputValue = 0
+                    , error = Nothing
+            }
 
         Clear ->
             initModel
         
-        ChangedInput val ->
-            { model | inputValue = val }
+        Input val ->
+            case toInt val of 
+                Ok num ->
+                    { model 
+                        | inputValue = num
+                        , error = Nothing 
+                    }
+
+                Err mess ->
+                    { model 
+                        | error = Just "It's not a number" 
+                    }
+
+valueInInput : Int -> String
+valueInInput inputValue = 
+    if inputValue == 0 then
+        ""
+    else
+        toString inputValue
 
 -- third part VIEW
 view : Model -> Html Msg
 view model = 
     let 
-        (helperString, disabledButton, val) =
-            case (String.toInt model.inputValue, length model.inputValue > 0) of
-                (Ok val, True) ->
-                    (toString val, False, val)
-                
-                (Err val, True) ->
-                    ("It's not a number.", True, 0)
+        (helperString, disabledButton, myVal) =
+            case model.error of 
+                Just mess ->
+                    (mess
+                    , True
+                    , valueInInput model.inputValue
+                    )
 
-                (Ok val, False) ->
-                    ("", False, 0)
-                
-                (Err val, False) ->
-                    ("", False, 0)
+                Nothing ->
+                    ("You want to add " ++ toString model.inputValue
+                    , False
+                    , valueInInput model.inputValue
+                    )
     in  
         div []
             [ h3
                 []
                 [ text ("Total calories : " ++ toString model.calories) ]
             , input 
-                [ onInput ChangedInput ]
+                [ onInput Input
+                , value myVal
+                ]
                 []
             , button
                 [ type' "button"
                 , disabled disabledButton
-                , onClick (AddCalorie val)
+                , onClick AddCalorie
                 ]
                 [ text "Add" ]
             , button
